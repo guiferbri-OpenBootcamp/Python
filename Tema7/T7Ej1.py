b70a7b2f132187049f7305983a279e422b400443
Guiomar <guio.fbb@gmail.com>
2022-07-12 11:10:14 +0200

    tema 7 ejercicio 1

---
+++ Tema7/T7Ej1.py
@@ -0,0 +1,12 @@
+from T7Ej1_operations import *
+
+num1 = 20
+num2 = 10
+add = add(num1, num2)
+sub = sub(num1, num2)
+mul = mul(num1, num2)
+div = div(num1, num2)
+print(f"Suma: {num1} + {num2} =", add)
+print(f"Resta: {num1} - {num2} =", sub)
+print(f"Multiplicación: {num1} * {num2} =", mul)
+print(f"División: {num1} / {num2} =", div)
---
+++ Tema7/T7Ej1_operations.py
@@ -0,0 +1,14 @@
+def add(elem1: float, elem2: float) -> float:
+    return float(elem1 + elem2)
+
+
+def sub(elem1: float, elem2: float) -> float:
+    return float(elem1 - elem2)
+
+
+def mul(elem1: float, elem2: float) -> float:
+    return float(elem1 * elem2)
+
+
+def div(elem1: float, elem2: float) -> float:
+    return float(elem1 / elem2)
